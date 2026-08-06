eac4aa40294229ddba4552cad139cc5519ede4e4
mfh2010-sudo <mfh2010@gmail.com>
2026-08-06 13:51:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // ⚠️ غيّر 'sales-crm' لاسم الـ repository بتاعك على GitHub
 export default defineConfig({
   plugins: [react()],
-  base: "/sales-crm/",
-});
+base: "/crm/",});
